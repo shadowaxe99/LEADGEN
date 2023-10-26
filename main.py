f346e7ed52ba39b061b88679b3c5f6faffadb529
Michael Gruen <michael.gruen9@gmail.com>
2023-10-26 11:24:38 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,123 +1,67 @@
-import streamlit as st
 
-from langchain.sql_database import SQLDatabase
-from langchain.llms.openai import OpenAI
-from requests import get, post
-from langchain.agents import AgentExecutor, load_tools, initialize_agent, Tool
-from langchain.tools.python.tool import PythonREPLTool
-from langchain.agents.agent_toolkits import create_python_agent
-from langchain.python import PythonREPL
-from langchain import SerpAPIWrapper
-from langchain.chains.conversation.memory import ConversationBufferMemory
-from langchain.prompts import PromptTemplate
-from langchain.chains import SimpleSequentialChain
-from langchain.chains import LLMChain
+# Streamlit code for a Fun and Intuitive Lead Generation App
 
-from dotenv import load_dotenv
-import requests
-from bs4 import BeautifulSoup
+# Import required modules for utility features
+import pandas as pd
+import random
 
-load_dotenv()
+# Title and description
+st.title("Fun & Intuitive Lead Generation App 🎉")
+st.write("Discover business opportunities in the most delightful way! 🚀")
 
-import os
+# Instructions
+st.write("### How to Use 🤔")
+st.write("Just fill in the search criteria below and hit the **Search** button. For a surprise, try the **I'm Feeling Lucky** button! 🍀")
 
-open_ai_key = os.environ.get("OPENAI_API_KEY")
-os.environ["SERPAPI_API_KEY"] = os.environ.get("SERPAPI_API_KEY")
+# Basic Input fields with placeholder text
+st.write("### Let's Find Some Leads! 🎯")
+industry_type = st.selectbox("Industry Type 🏭", ["", "Technology 🖥", "Healthcare 🏥", "Finance 💰", "Education 📚"])
+location = st.text_input("Location 🗺", placeholder="e.g., New York")
+company_size = st.selectbox("Company Size 👥", ["", "1-10 👨‍💻", "11-50 👩‍💼", "51-200 👩‍🔬", "201-500 👨‍🚀", "500+ 👩‍✈️"])
 
+# Fun Feature: I'm Feeling Lucky Button
+if st.button("I'm Feeling Lucky 🍀"):
+    st.write("Finding you a random lead... 🎲")
+    # Implement your random lead logic here (Example provided below)
+    random_lead = {
+        "Name": "Lucky Company",
+        "Location": "Neverland",
+        "Size": "1-10",
+        "Industry": "Fun & Games"
+    }
+    st.write(f"### Here's Your Lucky Lead! 🌟")
+    st.write(random_lead)
 
-def execute_query(query):
-    search = SerpAPIWrapper()
-    tools = [
-        Tool(
-            name="Search",
-            func=search.run,
-            description="useful for when you need to answer questions about current events",
-        )
-    ]
-    llm = OpenAI(temperature=0, openai_api_key=open_ai_key)
+# Search Button and Validation
+elif st.button("Search 🔍"):
+    if not industry_type:
+        st.warning("Please select an Industry Type. 🚫")
+    elif not location:
+        st.warning("Please enter a Location. 🚫")
+    else:
+        st.write(f"Searching for leads in the {industry_type} industry, located in {location}, with a company size of {company_size}. 🕵️‍♀️")
+        # Implement your search logic here (Example provided below)
+        example_leads = pd.DataFrame({
+            "Name": ["Company A", "Company B"],
+            "Location": ["New York", "San Francisco"],
+            "Size": ["1-10", "11-50"],
+            "Industry": ["Technology", "Healthcare"]
+        })
+        st.write("### Search Results 📋")
+        st.write(example_leads)
 
-    tools = load_tools(["serpapi", "llm-math"], llm=llm)
-    agent = initialize_agent(
-        tools, llm, agent="zero-shot-react-description", verbose=True
-    )
+# Reset Button to Clear Fields
+if st.button("Reset 🔄"):
+    industry_type = ""
+    location = ""
+    company_size = ""
+    st.experimental_rerun()
 
-    r = agent.run(query)
-    return r
+# Search History (Example)
+st.write("### Search History 🕰")
+st.write("1. Technology, San Francisco, 11-50")
+st.write("2. Healthcare, New York, 51-200")
 
-
-memory = ConversationBufferMemory(memory_key="chat_history")
-
-
-def scrape(urls):
-    print("urks", urls)
-    for url in urls:
-        response = requests.get(url, verify=True)
-        soup = BeautifulSoup(response.text, "html.parser")
-        contact_info = soup.find("div", {"class": "contact-info"})
-        print(contact_info)
-        return contact_info
-
-
-def find_and_save_leads(query):
-    python = PythonREPL()
-    tools = [
-        Tool(
-            name="Scrape",
-            func=lambda urls: scrape(urls),
-            description="function to scrape websites with python when you provide the exact url links",
-        )
-    ]
-    tool = Tool(
-        name="Scrape",
-        func=lambda urls: scrape(urls),
-        description="useful for when you need to scrape websites with python",
-    )
-    llm = OpenAI(temperature=0, openai_api_key=open_ai_key)
-
-    tools = load_tools(["serpapi", "python_repl"], llm=llm)
-    # tools.append(tool)
-    agent = initialize_agent(
-        tools, llm, agent="zero-shot-react-description", memory=memory, verbose=True
-    )
-    r = agent.run(query)
-    return r
-
-
-def find_leads(query):
-    llm = OpenAI(temperature=0.3, openai_api_key=open_ai_key)
-    template = """You are an expert in scraping websites with python.
-    Use your expertise to scrape the following websites for {contact}.
-    You can use the python REPL to find the tools.
-
-    Websites: {websites}
-    Era: {era}
-    Playwright: This is a synopsis for the above play:"""
-    prompt_template = PromptTemplate(
-        input_variables=["websites", "emails"], template=template
-    )
-
-
-st.header("Dune SQL Generator")
-
-user_input = st.text_input("Enter your SQL query here")
-
-if "memory" not in st.session_state:
-    st.session_state["memory"] = ""
-
-if st.button("Generate Dune SQL"):
-    st.markdown(find_and_save_leads(user_input))
-    st.session_state["memory"] += memory.buffer
-    print(st.session_state["memory"])
-
-# emails = []
-#               for url in urls.values():
-#                   response = requests.get(url)
-#                   soup = BeautifulSoup(response.text, 'html.parser')
-#                   for link in soup.find_all('a'):
-#                       if 'mailto:' in link.get('href'):
-#                           emails.append(link.get('href').split(':')[1])
-#               with open('emails.csv', 'w', newline='') as csvfile:
-#                   writer = csv.writer(csvfile)
-#                   writer.writerow(['Website', 'Email'])
-#                   for url, email in zip(urls.values(), emails):
-#                       writer.writerow([url, email])
+# Fun GIF for engagement
+st.write("### Having Fun Yet? 😄")
+st.image("https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif")
